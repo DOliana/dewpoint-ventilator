7d6aff11beca484121843c762bd10ea1291246e3
DOliana <DOliana@users.noreply.github.com>
2023-07-03 19:00:27 +0200

    Create secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,9 @@
+#define SECRET_WIFI_SSID "YOUR SSID"
+#define SECRET_WIFI_PASSWORD "YOUR PASSWORD"
+
+#define SECRET_MQTT_SERVER ""
+#define SECRET_MQTT_USER ""
+#define SECRET_MQTT_PASSWORD ""
+#define SECRET_MQTT_PORT 1883
+#define SECRET_MQTT_BASETOPIC "dewpoint-vent"
+#define SECRET_MQTT_CLIENT_ID "dewpoint-vent"
