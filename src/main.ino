ef078e3bea13c9a1811023d3162f440c20f3b223
Denis <DOliana@users.noreply.github.com>
2023-07-03 20:50:16 +0200

    add mqtt publishing

--- src/main.ino
+++ src/main.ino
@@ -3,19 +3,20 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <secrets.h>
+#include <PubSubClient.h>
 
 #define RELAIPIN 16 // connection for ventilator relais switch
 #define DHTPIN_1 13 // data line for DHT sensor 1 (inside)
 #define DHTPIN_2 14 // Datenleitung für den DHT-Sensor 2 (außen)
 
-#define RELAIS_EIN LOW
-#define RELAIS_AUS HIGH
-bool rel;
+#define RELAIS_ON LOW
+#define RELAIS_OFF HIGH
+bool ventilatorStatus;
 
 #define DHTTYPE_1 DHT22 // DHT 22
 #define DHTTYPE_2 DHT22 // DHT 22
 
-// ******* Correction values for individual sensor values *******
+// ******* Correction values for individual sensor values ***********
 #define CORRECTION_temp_1 0     // correction value for indoor sensor temperature
 #define CORRECTION_temp_2 0.1   // correction value for outdoor sensor temperature
 #define CORRECTION_humidity_1 0 // correction value for indoor sensor humidity
@@ -27,13 +28,35 @@ bool rel;
 #define TEMP1_min 10.0  // minimum indoor temperature at which ventilation is activated
 #define TEMP2_min -10.0 // minimum outdoor temperature at which ventilation is activated
 
-// ********* Wifi + MQTT settings *********
+// ********* Wifi + MQTT settings (values defined in secret.h) ******
+const char *mqtt_server = SECRET_MQTT_SERVER;
+const char *mqtt_user = SECRET_MQTT_USER;
+const char *mqtt_password = SECRET_MQTT_PASSWORD;
+#ifdef SECRET_MQTT_BASETOPIC
+const char *mqtt_baseTopic = SECRET_MQTT_BASETOPIC;
+#else
+const char *mqtt_baseTopic = "";
+#endif
+#ifdef SECRET_MQTT_CLIENT_ID
+const char *mqtt_clientID = SECRET_MQTT_CLIENT_ID;
+#else
+const char *mqtt_clientID = "dewpoint-vent-client";
+#endif
+#ifndef SECRET_MQTT_PORT
+const int mqtt_port = 1883;
+#else
+const int mqtt_port = SECRET_MQTT_PORT;
+#endif
+
 const char *ssid = SECRET_WIFI_SSID;
 const char *password = SECRET_WIFI_PASSWORD;
-const char *wifi_hostname = "dewpoint-vent";
+const char *wifi_hostname = mqtt_clientID;
+//*******************************************************************
 
 int wifiErrorCounter = 0;
-// ****************************************
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 DHT dht1(DHTPIN_1, DHTTYPE_1); // The indoor sensor is now addressed with dht1
 DHT dht2(DHTPIN_2, DHTTYPE_2); // The outdoor sensor is now addressed with dht2
@@ -45,9 +68,9 @@ void setup()
     ESP.wdtEnable(WDTO_8S);
 
     pinMode(LED_BUILTIN, OUTPUT);       // Define LED pin as output
-    digitalWrite(0, HIGH); // Turn on LED to show some activity
+    digitalWrite(0, HIGH);              // Turn on LED to show some activity
     pinMode(RELAIPIN, OUTPUT);          // Define relay pin as output
-    digitalWrite(RELAIPIN, RELAIS_AUS); // Turn off relay
+    digitalWrite(RELAIPIN, RELAIS_OFF); // Turn off relay
 
     Serial.begin(115200);
     Serial.println(F("Testing sensors..."));
@@ -58,7 +81,7 @@ void setup()
 
 void loop()
 {
-    digitalWrite(0, HIGH); // Turn on LED to show some activity
+    digitalWrite(0, HIGH);                                  // Turn on LED when loop is active
     float h1 = dht1.readHumidity() + CORRECTION_humidity_1; // Read indoor humidity and store it under "h1"
     float t1 = dht1.readTemperature() + CORRECTION_temp_1;  // Read indoor temperature and store it under "t1"
     float h2 = dht2.readHumidity() + CORRECTION_humidity_2; // Read outdoor humidity and store it under "h2"
@@ -96,7 +119,7 @@ void loop()
 
     if (errorOnInitialize == true)
     {
-        digitalWrite(RELAIPIN, RELAIS_AUS); // Turn off ventilator
+        digitalWrite(RELAIPIN, RELAIS_OFF); // Turn off ventilator
         Serial.println(F("Restarting..."));
         while (1)
             ; // Endless loop to restart the CPU through the watchdog
@@ -104,41 +127,9 @@ void loop()
     // feed the watchdog
     ESP.wdtFeed();
 
-    //********* Wifi connection *********
-    // if connection is unsuccessful, try again every 20 loops
-    if (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.println(F("WiFi disconnected"));
-        if (wifiErrorCounter <= 0)
-        {
-            Serial.println(F("connecting to WiFi..."));
-            WiFi.setHostname(wifi_hostname);
-            WiFi.begin(ssid, password);
-            int waitCounter = 0;
-            while (WiFi.status() != WL_CONNECTED && waitCounter < 10)
-            {
-                delay(500);
-                Serial.print(".");
-                waitCounter++;
-            }
+    connectWifiIfNecessary();
 
-            if (WiFi.status() != WL_CONNECTED)
-            {
-                Serial.println(F("WiFi connection failed"));
-                wifiErrorCounter = 20;
-            }
-            else
-            {
-                Serial.println(F("WiFi connected"));
-            }
-        }
-        else
-        {
-            wifiErrorCounter--;
-        }
-    }
-
-    //**** Calculate dew points********
+    //**** Print sensor values********
     Serial.print(F("Sensor-1: "));
     Serial.print(F("hunidity: "));
     Serial.print(h1);
@@ -153,10 +144,12 @@ void loop()
     Serial.print(t2);
     Serial.println(F("°C"));
 
+    //**** Calculate dew points********
     Serial.println(F("calculating dew point..."));
     float dewPoint_1 = calculateDewpoint(t1, h1);
     float dewPoint_2 = calculateDewpoint(t2, h2);
 
+    //**** Print dew points********
     Serial.print(F("Sensor-1 dew point: "));
     Serial.print(dewPoint_1);
     Serial.println(F("°C  "));
@@ -167,33 +160,59 @@ void loop()
 
     ESP.wdtFeed();
 
+    //**** Calculate difference between dew points********
     float DeltaTP = dewPoint_1 - dewPoint_2;
 
+    //**** decide if ventilator should run or not ********
     if (DeltaTP > (MIN_Delta + HYSTERESE))
-        rel = true;
+        ventilatorStatus = true;
     if (DeltaTP < (MIN_Delta))
-        rel = false;
+        ventilatorStatus = false;
     if (t1 < TEMP1_min)
-        rel = false;
+        ventilatorStatus = false;
     if (t2 < TEMP2_min)
-        rel = false;
+        ventilatorStatus = false;
 
-    if (rel == true)
+    if (ventilatorStatus == true)
     {
-        digitalWrite(RELAIPIN, RELAIS_EIN); // Turn on relay
+        digitalWrite(RELAIPIN, RELAIS_ON); // Turn on relay
         Serial.println(F("-> ventilation ON"));
     }
     else
     {
-        digitalWrite(RELAIPIN, RELAIS_AUS); // Turn off relay
+        digitalWrite(RELAIPIN, RELAIS_OFF); // Turn off relay
         Serial.println(F("-> ventilation OFF"));
+    }
+
+    // **** publish vlaues via MQTT ********
+    if (client.connected())
+    {
+        String baseTopic = "";
+#ifdef SECRET_MQTT_BASETOPIC
+        baseTopic = mqtt_baseTopic;
+        if (baseTopic.endsWith("/") == false)
+        {
+            baseTopic.concat("/");
+        }
+#endif
+        Serial.print("Publishing to ");
+        Serial.println(baseTopic);
+        client.publish((baseTopic + "sensor-1/temperature").c_str(), String(t1, 0).c_str());
+        client.publish((baseTopic + "sensor-1/humidity").c_str(), String(h1, 0).c_str());
+        client.publish((baseTopic + "sensor-1/dewpoint").c_str(), String(dewPoint_1, 0).c_str());
+        client.publish((baseTopic + "sensor-2/temperature").c_str(), String(t2, 0).c_str());
+        client.publish((baseTopic + "sensor-2/humidity").c_str(), String(h2, 0).c_str());
+        client.publish((baseTopic + "sensor-2/dewpoint").c_str(), String(dewPoint_2, 0).c_str());
+        client.publish((baseTopic + "ventilation/state").c_str(), ventilatorStatus ? "ON" : "OFF");
+        client.publish((baseTopic + "ventilation/stateNum").c_str(), ventilatorStatus ? "1" : "0");
+        Serial.println(F("published to MQTT"));
     }
 
     Serial.println();
 
-    delay(4000);
-    digitalWrite(BUILTIN_LED, LOW); // Turn off LED
-    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW); // Turn off LED while sleeping
+
+    delay(5000);
     ESP.wdtFeed();
 }
 
@@ -224,4 +243,107 @@ float calculateDewpoint(float t, float r)
     // dew point temperature (°C)
     float tt = (b * v) / (a - v);
     return {tt};
+}
+
+// This function checks if the WiFi is connected and tries to connect if it is not.
+// If the connection is unsuccessful, it will retry every 20 loops.
+void connectWifiIfNecessary()
+{
+    // if connection is unsuccessful, try again every 20 loops
+    if (WiFi.status() != WL_CONNECTED)
+    {
+#ifdef SECRET_WIFI_SSID &&SECRET_WIFI_PASSWORD
+        Serial.println(F("WiFi disconnected"));
+        if (wifiErrorCounter <= 0)
+        {
+            Serial.println(F("connecting to WiFi..."));
+            WiFi.setHostname(wifi_hostname);
+            WiFi.begin(ssid, password);
+            int waitCounter = 0;
+            while (WiFi.status() != WL_CONNECTED && waitCounter < 10)
+            {
+                blinkDelay(500);
+                digitalWrite(LED_BUILTIN, HIGH);
+                Serial.print(".");
+                waitCounter++;
+            }
+
+            if (WiFi.status() != WL_CONNECTED)
+            {
+                Serial.println(F("WiFi connection failed"));
+                wifiErrorCounter = 20;
+            }
+            else
+            {
+                Serial.println(F("WiFi connected"));
+                connectMQTTIfNecessary();
+            }
+        }
+        else
+        {
+            wifiErrorCounter--;
+            Serial.print("retrying wifi connection in ");
+            Serial.print(wifiErrorCounter);
+            Serial.println(" loops");
+        }
+#else
+        Serial.println("WiFi not configured");
+#endif
+    }
+    else
+    {
+        // wifi connection continues even if we don't wait for it - we need to check periodically
+        // even if WiFi connection failed in the first place.
+        connectMQTTIfNecessary();
+    }
+}
+
+// This function connects to the MQTT server if necessary
+void connectMQTTIfNecessary()
+{
+    // Check if WiFi is connected
+    if (WiFi.status() == WL_CONNECTED)
+    {
+// Check if necessary MQTT credentials are defined
+#ifndef SECRET_MQTT_USER
+#error "SECRET_MQTT_USER not defined"
+#endif
+#ifndef SECRET_MQTT_PASSWORD
+#error "SECRET_MQTT_PASSWORD not defined"
+#endif
+#ifndef SECRET_MQTT_SERVER
+#error "SECRET_MQTT_SERVER not defined"
+#endif
+#ifndef SECRET_MQTT_CLIENT_ID
+#error "SECRET_MQTT_CLIENT_ID not defined"
+#endif
+
+        // Check if client is connected to MQTT server, if not, connect
+        if (client.connected() == false)
+        {
+            Serial.println(F("WiFi connected. Connecting to MQTT..."));
+            client.setServer(mqtt_server, mqtt_port);
+            if (client.connect(mqtt_clientID, mqtt_user, mqtt_password))
+            {
+                Serial.println(F("MQTT connected"));
+            }
+            else
+            {
+                Serial.println(F("MQTT connection failed"));
+            }
+        }
+    }
+}
+
+// This function blinks the built-in LED with a given delay time
+void blinkDelay(int delayTime)
+{
+    while (delayTime > 0)
+    {
+        digitalWrite(LED_BUILTIN, HIGH); // turn on the LED
+        delay(50);                       // wait for 50ms
+        digitalWrite(LED_BUILTIN, LOW);  // turn off the LED
+        delay(50);                       // wait for 50ms
+        delayTime -= 100;                // subtract 100ms from the delay time
+    }
 }
